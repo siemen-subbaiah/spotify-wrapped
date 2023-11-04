a8d438a6d3eb808e8db2134d6f7741bc29b89255
siemen_subbaiah <siemen.s@iqnext.io>
2023-11-04 20:14:13 +0530

    title change and done

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -7,7 +7,7 @@ const NavBar = () => {
   const accessToken = cookieStore.get('accessToken')?.value;
   return (
     <nav className='px-4 md:px-40 py-5 flex items-center justify-between'>
-      <h1 className='text-2xl'>Spotify API</h1>
+      <h1 className='text-2xl'>Spotify Dev Wrapped</h1>
       {accessToken && <Logout />}
     </nav>
   );
